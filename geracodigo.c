8486285fc496758cca16f10939a88a750a0a95c5
Murilo Martin <murilodsmartin@gmail.com>
2022-11-19 01:33:43 -0300

    wip geraCodigo function
    started switch to verify souce code. Also created "impletação1" to hepl with testing

--- geracodigo.c
+++ geracodigo.c
@@ -1,3 +1,7 @@
 //Arquivo para a entrga do trabalho (já com o nome certo kk)
 //Murilo_Martin   1921063 33B
 //Stella_Maranhão xxxxxxx 33B
+
+#include <stdio.h>
+typedef int (*funcp) ();
+funcp geraCodigo(FILE *f, unsigned char codigo[]);
--- testes.c
+++ testes.c
@@ -1,1 +1,46 @@
 //arquivo para testar testar as funções do trabalho
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXT 1000
+
+typedef int (*funcp) ();
+
+//Protótipos:
+funcp geraCodigo (FILE *f, unsigned char codigo[]);
+static void error (const char *msg, int line);
+
+int main(void){
+
+    FILE * fp = fopen("implementacao1.txt","r");
+    unsigned char Vetor[MAXT];
+    geraCodigo(fp,Vetor);
+    return 0;
+}
+
+funcp geraCodigo(FILE *f, unsigned char codigo[]){
+    funcp Funcp;
+    char c;
+    int line =1;
+
+    while ((c = fgetc(f)) != EOF) {
+        // printf("%c\n",c); //verificando se estava lendo letra por letra
+        switch(c){
+            case 'r':;
+                char var0;
+                int idx0;
+                 if (fscanf(f, "et %c%d", &var0, &idx0) != 2)
+                    error("comando invalido", line);
+                else
+                    printf("caso de retorno -> ok");
+                break;
+            default: error("comando desconhecido", line);
+        }
+    }
+    return Funcp;
+}
+
+static void error (const char *msg, int line) {
+  fprintf(stderr, "erro %s na linha %d\n", msg, line);
+  exit(EXIT_FAILURE);
+}
